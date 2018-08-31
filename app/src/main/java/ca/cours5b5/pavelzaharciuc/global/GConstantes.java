eb3c6f19817b9b95eef547af9a68e26fd84e1e7d
zhcmon18 <zhcmon18@gmail.com>
2018-08-31 11:03:59 -0400

    Correction d'une erreur.

--- app/src/main/java/ca/cours5b5/pavelzaharciuc/global/GConstantes.java
+++ app/src/main/java/ca/cours5b5/pavelzaharciuc/global/GConstantes.java
@@ -7,8 +7,8 @@ public final class GConstantes
    public static final int HEIGHT_DEFAULT_POS = 2;
 
    public static final int WIDTH_MIN = 4;
-   public static final int WIDTH_MAX = 7;
-   public static final int WIDTH_DEFAULT_POS = 1;
+   public static final int WIDTH_MAX = 10;
+   public static final int WIDTH_DEFAULT_POS = 3;
 
    public static final int TO_WIN_MIN = 3;
    public static final int TO_WIN_MAX = 4;
