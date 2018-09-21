5ae455e290e42bb898202c2d2c1e52cd659e141b
zhcmon18 <zhcmon18@gmail.com>
2018-09-21 13:38:15 -0400

    FAIT: * Création de la structure de l'atelier 6 TODO: * Création et implémentation des méthodes

---
+++ app/src/main/java/ca/cours5b5/pavelzaharciuc/controleurs/Action.java
@@ -0,0 +1,25 @@
+package ca.cours5b5.pavelzaharciuc.controleurs;
+
+import ca.cours5b5.pavelzaharciuc.controleurs.interfaces.Fournisseur;
+import ca.cours5b5.pavelzaharciuc.controleurs.interfaces.ListenerFournisseur;
+
+public class Action {
+
+    Fournisseur fournisseur;
+
+    ListenerFournisseur listenerFournisseur;
+
+    Object[] args;
+
+    public void setArguments(Object... args) {
+
+    }
+
+    public void executerDesQuePossible() {
+
+    }
+
+    public Action cloner() {
+        return null;
+    }
+}
---
+++ app/src/main/java/ca/cours5b5/pavelzaharciuc/global/GCommande.java
@@ -0,0 +1,11 @@
+package ca.cours5b5.pavelzaharciuc.global;
+
+public enum GCommande {
+
+    CHOISIR_HAUTEUR,
+    CHOISIR_LARGEUR,
+    CHOISIR_POUR_GAGNER,
+
+    OUVRIR_MENU_PARAMET
+
+}
